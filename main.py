7af027b70d3d650c8fac59c20bf01011d3e2a09c
Monty-CB <monty.codercb@gmail.com>
2025-07-15 20:06:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import server
+import uvicorn
+from app.server import app  # Importar la app FastAPI
 
-if __name__ == '__main__':
-    server.start_server()
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
